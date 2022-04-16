266df2a1a85686af9a8c59da48b51b7cec2c153a
Lelius <leliuslogunov@gmail.com>
2022-04-16 15:02:12 +0300

    Добавил два фрагмента индикации и управления. Сделал в них разметку полей и кнопок. Добавил эти фрагменты с разметкой в activity_main.xml. Теперь буду все это пытаться оживить. :)

---
+++ app/src/main/java/com/example/echodreams/IndicatorFragment.java
@@ -0,0 +1,21 @@
+package com.example.echodreams;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IndicatorFragment extends Fragment {
+
+    public IndicatorFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/echodreams/MainActivity.java
+++ app/src/main/java/com/example/echodreams/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.echodreams;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
